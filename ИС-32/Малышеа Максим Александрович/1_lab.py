dd1c9a8d6c17a4cf78ebaf57a7cccb4df44760b8
Qutigara <92624921+Qutigara@users.noreply.github.com>
2021-10-16 12:37:19 +0300

    [1] Начал делать 1 задание
    Создал Картежи словари и прогнал их через генераторы

--- ИС-32/Малышеа Максим Александрович/1_lab.py
+++ ИС-32/Малышеа Максим Александрович/1_lab.py
@@ -6,8 +6,7 @@ tuple_list = (3,1,542,"5425",'rgrerdtfg',.542425,None)
 
 dict_list = {"int": 131, 'float': -1.431, 'str': "gfdgsf", 'complex': (5+1/3j), 'ept': None}
 
-filt_dict = {key: value for key, value in dict_list.items() if len(key)>3 or value != None}
+filt_di = {key: value for key, value in dict_list.items() if len(key)>3 or value != None}
 
-
-print(filt_dict)
+print(filt_di)
 print(filtered_list)
